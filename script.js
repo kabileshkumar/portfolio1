edc0887a7e07d4ef538ecc72332da69c9158fd5c
Kabilesh Kumar S <141045803+kabileshkumar@users.noreply.github.com>
2024-08-14 19:01:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
